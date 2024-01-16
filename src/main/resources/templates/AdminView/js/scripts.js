f89e2cdd83ba14d65799f561510a7184296b2d6a
Kamil Bundyra <kamilbundyra@gmail.com>
2024-01-16 15:59:09 +0100

    Panel admina - html i CSS

---
+++ src/main/resources/templates/AdminView/js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+//document.addEventListener('DOMContentLoaded', function () {
+//        // Inicjalizacja karuzeli
+//        var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
+//            interval: 5000,  // Czas trwania każdego slajdu w milisekundach (w tym przypadku 5 sekund)
+//            wrap: true,      // Zapętlanie karuzeli
+//            keyboard: true   // Obsługa klawiatury
+//        });
+//    });
